c025db19957cee2ed44dacee6366c315ca2ebd47
Colin Rosati <colin.james.rosati@gmail.com>
2019-10-25 19:37:44 +0200

    adding index nax link iterater UI

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,21 @@
-import React from 'react'; 
+import React, { useState, Component } from 'react'; 
+import ReactDom from 'react-dom';
 import tapOrClick from 'react-tap-or-click'
 import '../styles/index.css'
 import {
   BrowserRouter as Router,
   Route,
 } from "react-router-dom";
+import Badge from 'react-bootstrap/Button';
+
 import IndexSite from "./index"
 
 import closeImg from '../assets/cancel-button.png'
 
 const IndexPage = () => {
+  var x = 1;
+  var y = 1;
+  var z = 1;
   
   function CloseImage(props) {
     return <img src={closeImg} className="closeButton" alt=""></img>
@@ -17,16 +23,11 @@ const IndexPage = () => {
 
   function CloseHead(props) {
 
-    // const handleEnter = (e) => {
-    //     let close =  document.getElementsByClassName('close-wrapper')
-    //     var x = document.getElementsByClassName('closeButton')[0]
-    //     console.log(x)
-    //     x.classlist.add("rotate")
-    // }
     
     const handleClick = (event) => {
       // event.preventDefault();
       document.body.classList.add("hide")
+      document.getElementsByClassName("closeButton").className.add('closeRotate')
       setTimeout(() => {
         window.location = "/";
       }, 500)
@@ -56,8 +57,22 @@ const IndexPage = () => {
       console.log("hovering")
   }
 
+  // function HoverCount(props, x){
+  //   // var [itr, setItr] = useState(1)
+  //   // setItr(itr++)
+  //   console.log("Vover Counter", props)
+  //   x++
+
+  //   return x
+  // }
+
   function LinkTitle(url) {
-      return <a href={url.href} style={linkStyle} >{url.children}</a>
+      //  url.onMouseEnter(url, x)
+      return(
+      <div className="NavLinkWrapper">
+        <a href={url.href} style={linkStyle} >{url.children}</a>
+      </div>
+      )
   }
 
 
@@ -65,19 +80,69 @@ const IndexPage = () => {
       return <a href={url.href} style={linkStyleHover}>{url.children}</a>
   }
 
-    function Nav (props){
-      var aboutL = "about";
-      var workL = "work";
-      var contactL = "contact";
+    class Nav extends Component{
+      constructor(props){
+        // this.props = props
+        super(props)
+        this.aboutL = "about";
+        this.workL = "work";
+        this.contactL = "contact";
+        this.i = 1
+        this.state = {
+          
+          x: 1,
+          y: 1,
+          z: 1         
+        };
+        this.HoverCount = this.HoverCount.bind(this);
+        this.handleTouchStart = this.handleTouchStart.bind(this);
+      }
+   
+      HoverCount(e){
+        if(e.target.innerHTML == 'About'){
+          this.setState({
+            x: x++,
+          });
+        }else if(e.target.innerHTML == 'Work'){
+          this.setState({
+            y: y++,
+          });
+        }else if(e.target.innerHTML == 'Contact'){
+          this.setState({
+            z: z++,
+          });
+        }
+      }
 
-      return(
+      handleTouchStart(e){
+        if(e.target.innerHTML == 'About'){
+          this.setState({
+            x: x++,
+          });
+        }else if(e.target.innerHTML == 'Work'){
+          this.setState({
+            y: y++,
+          });
+        }else if(e.target.innerHTML == 'Contact'){
+          this.setState({
+            z: z++,
+          });
+        }
+      }
+    
+      render(){
+        var aboutL = "about";
+        var workL = "work";
+        var contactL = "contact";
+        return(
           <div  className="content">
           <HeadTitle>Colin Rosati</HeadTitle>
-          <LinkTitle href={aboutL} className="navLink" onHover={hovering}><h2 className="nav">About</h2></LinkTitle>
-          <LinkTitle href={workL} className="navLink"><h2 className="nav">Work</h2></LinkTitle>
-          <LinkTitle href={contactL} className="navLink" ><h2 className="nav">Contact</h2></LinkTitle>
+          <LinkTitle href={this.aboutL} className="navLink"><h2 className="nav" onTouchStart={(e) => this.handleTouchStart(e)} onMouseEnter={(e) => this.HoverCount(e)}>About</h2><Badge variant="secondary">{this.state.x}</Badge></LinkTitle>
+          <LinkTitle href={workL} className="navLink" ><h2 className="nav" onTouchStart={(e) => this.handleTouchStart(e)} onMouseEnter={(e) => this.HoverCount(e)}>Work</h2><Badge variant="secondary">{this.state.y}</Badge></LinkTitle>
+          <LinkTitle href={contactL} className="navLink" ><h2 className="nav" onTouchStart={(e) => this.handleTouchStart(e)} onMouseEnter={(e) => this.HoverCount(e)}>Contact</h2><Badge variant="secondary">{this.state.z}</Badge></LinkTitle>
           </div>
       )
+      }
     }
 
     const AppBody = ()=>{
