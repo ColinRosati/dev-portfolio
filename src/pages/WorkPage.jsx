d3782ef3c7b3be9770f6fb908c461d57366c13ee
Colin Rosati <colin.james.rosati@gmail.com>
2019-11-08 10:36:13 +0100

    update broken work links

--- src/pages/WorkPage.jsx
+++ src/pages/WorkPage.jsx
@@ -64,12 +64,12 @@ const WorkPage = () => {
     return(
       <div>
         <WorkHead/>
-        <Projects head={"Bunker3"} link={"www.bunker3.ca"} linkTitle={"Bunker3"} details={"Design, Development → D3.js, HTML, ES6"}/>
-        <Projects head={"Agar Studio"} link={"www.agarstudio.com"} linkTitle={"AGAR"} details={"Development → Wordpress, ES6"}/>
+        <Projects head={"Bunker3"} link={"https://www.bunker3.ca"} linkTitle={"Bunker3"} details={"Design, Development → D3.js, HTML, ES6"}/>
+        <Projects head={"Agar Studio"} link={"http://www.agarstudio.com"} linkTitle={"AGAR"} details={"Development → Wordpress, ES6"}/>
         <Projects head={"FTI Scope Data Collector web app"} link={"https://github.com/ColinRosati1/data_collector"} linkTitle={"Raspberry PI Server"} details={"Design, Development  →  Node.js, Bash scripts"}/>
-        <Projects head={"Custom Contact Page"} link={"http://agarstudio.com/wp-content/themes/agar/avalanche/agar-avalanche.html"}  linkTitle={"AGAR"} details={"Development → Worpress, ES6"}/>
-        <Projects head={"Gnothi Sauton"} link={"gnothisauton.xyz"} linkTitle={"Gnothi Sauton"} details={"Design, Development → HTML, Bootstrap, ES6"}/>
-        <Projects head={"Whatify"} link={"whatify.world"} linkTitle={"Whatify"} details={"Design, Development → HTML, Bootstrap.js, FaceBook API"}/>
+        <Projects head={"Custom Contact Page"} link={"http://www.agarstudio.com/wp-content/themes/agar/avalanche/agar-avalanche.html"}  linkTitle={"AGAR"} details={"Development → Worpress, ES6"}/>
+        <Projects head={"Gnothi Sauton"} link={"https://www.gnothisauton.xyz"} linkTitle={"Gnothi Sauton"} details={"Design, Development → HTML, Bootstrap, ES6"}/>
+        <Projects head={"Whatify"} link={"https://www.whatify.world"} linkTitle={"Whatify"} details={"Design, Development → HTML, Bootstrap.js, FaceBook API"}/>
       </div>
     )
   }
