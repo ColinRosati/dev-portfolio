a94aedbe733f3150c86f543ef3ade0e415c49230
ColinRosati1 <colin.rosati@finiata.com>
2021-03-27 14:09:10 +0100

    Updates

---
+++ precache-manifest.e0668fe9e81c6ae25dedd6ea4bcfb4e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f6a700e6992ddcd5e1be497d2b0d948",
+    "url": "/dev-portfolio/index.html"
+  },
+  {
+    "revision": "7755e985f57f0372f330",
+    "url": "/dev-portfolio/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "1a9da152193c417ddc7d",
+    "url": "/dev-portfolio/static/css/main.75b7b9fc.chunk.css"
+  },
+  {
+    "revision": "7755e985f57f0372f330",
+    "url": "/dev-portfolio/static/js/2.648e66ed.chunk.js"
+  },
+  {
+    "revision": "1a9da152193c417ddc7d",
+    "url": "/dev-portfolio/static/js/main.b0ae5f10.chunk.js"
+  },
+  {
+    "revision": "213ee944dada9a44e826",
+    "url": "/dev-portfolio/static/js/runtime-main.c8bfeb32.js"
+  },
+  {
+    "revision": "38321d9d10fb56f9311859c8dfea764c",
+    "url": "/dev-portfolio/static/media/cancel-button.38321d9d.png"
+  },
+  {
+    "revision": "f11f18747b64f95604055f72a3565162",
+    "url": "/dev-portfolio/static/media/headshot.f11f1874.png"
+  }
+]);
