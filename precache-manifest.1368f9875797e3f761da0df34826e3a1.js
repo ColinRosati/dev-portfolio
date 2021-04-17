20ce9d7595a9b5cfe9a60040fda603f46cd1bf71
ColinRosati1 <colin.rosati@finiata.com>
2021-04-17 22:14:39 +0200

    Updates

---
+++ precache-manifest.1368f9875797e3f761da0df34826e3a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a753f419fd74024c02a81f9236627c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e56f4be0379235ae978",
+    "url": "/static/css/2.10583f31.chunk.css"
+  },
+  {
+    "revision": "84b8daf46dfe94e517cf",
+    "url": "/static/css/main.b7ff967b.chunk.css"
+  },
+  {
+    "revision": "2e56f4be0379235ae978",
+    "url": "/static/js/2.da579c7f.chunk.js"
+  },
+  {
+    "revision": "84b8daf46dfe94e517cf",
+    "url": "/static/js/main.45140888.chunk.js"
+  },
+  {
+    "revision": "0c52c8980eaea7e66459",
+    "url": "/static/js/runtime-main.f26494fa.js"
+  },
+  {
+    "revision": "38321d9d10fb56f9311859c8dfea764c",
+    "url": "/static/media/cancel-button.38321d9d.png"
+  },
+  {
+    "revision": "f11f18747b64f95604055f72a3565162",
+    "url": "/static/media/headshot.f11f1874.png"
+  }
+]);
