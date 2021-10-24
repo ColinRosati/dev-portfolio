260e534e7f3995f80925dcf08d6dfa5aceefc262
ColinRosati1 <colin.rosati@finiata.com>
2021-10-24 20:20:59 +0200

    fix url path

--- src/pages/WorkPage/index.jsx
+++ src/pages/WorkPage/index.jsx
@@ -12,7 +12,7 @@ const WorkPage = () => {
         <h1>Recent Projects</h1>
         <Projects
           head={'Universal Basic Income Directory'}
-          link={'https://colinrosati1.github.io/ubi/'}
+          link={'https://colinrosati.github.io/ubi/'}
           linkTitle={'Universal Basic Income Directory'}
           details={
             'Design, Development → React, Typescript, Storybook, SCSS, HTML, NPM, Webpack '
@@ -20,7 +20,7 @@ const WorkPage = () => {
         />
         <Projects
           head={'Sensor Data visualization App'}
-          link={'https://github.com/ColinRosati1/smart-steel-app'}
+          link={'https://github.com/ColinRosati/smart-steel-app'}
           linkTitle={'Sensor Data visualization App'}
           details={
             'Design, Development → React, Highcharts, HTML, NPM, Jest, Python, Flask'
@@ -28,7 +28,7 @@ const WorkPage = () => {
         />
         <Projects
           head={'Hotel finder App'}
-          link={'https://github.com/ColinRosati1/hotelpirates'}
+          link={'https://github.com/ColinRosati/hotelpirates'}
           linkTitle={'Hotel Finder App'}
           details={'Design, Development → ES6, HTML, Webpack, Jest, Puppeteer'}
         />
@@ -40,7 +40,7 @@ const WorkPage = () => {
         />
         <Projects
           head={'FTI Scope Data Collector web app'}
-          link={'https://github.com/ColinRosati1/data_collector'}
+          link={'https://github.com/ColinRosati/data_collector'}
           linkTitle={'Raspberry PI Server'}
           details={'Design, Development  →  Node.js, Bash scripts'}
         />
