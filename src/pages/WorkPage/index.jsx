ac9dfc569b6dbf4bc21dcaf256ce3c9109b9fba1
ColinRosati1 <colin.rosati@finiata.com>
2021-10-24 14:32:30 +0200

    Update work page

--- src/pages/WorkPage/index.jsx
+++ src/pages/WorkPage/index.jsx
@@ -39,36 +39,10 @@ const WorkPage = () => {
           details={'Design, Development → D3.js, HTML, ES6'}
         />
         <Projects
-          head={'Agar Studio'}
-          link={'http://www.agarstudio.com'}
-          linkTitle={'AGAR'}
-          details={'Development → Wordpress, ES6'}
-        />
-        <Projects
           head={'FTI Scope Data Collector web app'}
           link={'https://github.com/ColinRosati1/data_collector'}
           linkTitle={'Raspberry PI Server'}
           details={'Design, Development  →  Node.js, Bash scripts'}
-        />
-        <Projects
-          head={'Custom Contact Page'}
-          link={
-            'http://www.agarstudio.com/wp-content/themes/agar/avalanche/agar-avalanche.html'
-          }
-          linkTitle={'AGAR'}
-          details={'Development → Worpress, ES6'}
-        />
-        <Projects
-          head={'Gnothi Sauton'}
-          link={'https://www.gnothisauton.xyz'}
-          linkTitle={'Gnothi Sauton'}
-          details={'Design, Development → HTML, Bootstrap, ES6'}
-        />
-        <Projects
-          head={'Whatify'}
-          link={'https://www.whatify.world'}
-          linkTitle={'Whatify'}
-          details={'Design, Development → HTML, Bootstrap.js, FaceBook API'}
         />
       </div>
     </div>
