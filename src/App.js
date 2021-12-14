637e96ef16af351ff5d5dff2e92b5b754e0804f4
Colin Rosati <colin.rosati@finiata.com>
2021-12-14 19:02:43 +0400

    clean up some lint errors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { CSSTransition } from 'react-transition-group';
@@ -12,11 +12,9 @@ import store from './store/store';
 import styles from './index.scss';
 
 export default () => {
-  const [onEnter, setOnEnter] = useState(false);
-
     return (
       <Provider store={store}>
-         <CSSTransition in={onEnter} timeout={1000} classNames={{ ...styles }}>
+         <CSSTransition in={false} timeout={1000} classNames={{ ...styles }}>
          <Router basename={'/dev-portfolio/'}>
          <Route render={({ location }) => (
             <Switch location={location}>
